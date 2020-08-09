1346044857480108708fc911f60e97ab22f48bfb
royliao <royliao88@gmail.com>
2020-08-09 13:33:34 +0800

    酸鹼色值表

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
+router.get('/ph', function(req, res, next) {
+  res.render('ph_table');
+});
 module.exports = router;
