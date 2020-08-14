6cfeed85a5134d5569ee53d8c8fe6ccffdb7f33c
royliao <royliao88@gmail.com>
2020-08-14 18:40:53 +0800

    ppm對照表

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/map', function(req, res, next) {
   res.render('index');
 });
 router.get('/ph', function(req, res, next) {
@@ -11,5 +11,8 @@ router.get('/ph', function(req, res, next) {
 router.get('/chart',function(req, res, next){
   console.log(req.query.chart);
   res.render('chart',{'query':req.query.chart});
+});
+router.get('/ppm',function(req, res, next){
+  res.render('ppm_table');
 })
 module.exports = router;
